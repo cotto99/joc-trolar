9adfc0bda7c6a84d758eee73d6caeff59dda3e61
bryan <bryancotto98@gmail.com>
2026-03-20 16:38:11 -0600

    correccion borrar bodega

--- resources/js/Pages/Bodegas/Index.vue
+++ resources/js/Pages/Bodegas/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
-import { useForm, Head } from '@inertiajs/vue3'
+import { useForm, Head, router } from '@inertiajs/vue3'
 import { ref } from 'vue'
 import Swal from 'sweetalert2'
 
